788f865a36e11ba6fe76483b14fb5309dd5e8aa9
Stefanomarton <sstefanomarton@gmail.com>
2024-07-24 00:55:53 +0200

    feat: create class

--- nocodb.py
+++ nocodb.py
@@ -2,91 +2,89 @@ import requests
 import json
 from datetime import datetime
 
-# Define the NocoDB URL components
-hosturl = "http://10.1.0.170:8080"
-port = 8080
-api_token = "jkjlH0Jk_3i_p4h5W5tZ7pCtx8RtJv9I76kCNRvR"
 
+class NocoDB:
+    def __init__(self, hosturl: str, port: int, api_token: str):
+        self.hosturl = hosturl
+        self.port = port
+        self.api_token = api_token
+        self.headers = {"xc-token": api_token}
 
-def getTableEntries(tableId: str, viewID: str = "") -> str:
-    """
-    Get entries for table with {tableId}, optionally use a view with {viewID}
-    """
-    url = f"{hosturl}/api/v2/tables/{tableId}/records"
+    def getTableEntries(self, tableId: str, viewID: str = "") -> str:
+        """
+        Get entries for table with {tableId}, optionally use a view with {viewID}
+        """
+        url = f"{self.hosturl}/api/v2/tables/{tableId}/records"
 
-    querystring = {
-        "viewId": viewID,
-    }
+        querystring = {
+            "viewId": viewID,
+        }
 
-    headers = {"xc-token": api_token}
+        headers = {"xc-token": self.api_token}
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    return response.text
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        return response.text
 
+    def createTableRow(self, tableId: str, data: dict[str, str]):
+        """
+        Create entry for table with {tableId}
+        """
+        url = f"{self.hosturl}/api/v2/tables/{tableId}/records"
 
-def createTableRow(tableId: str, data: dict[str, str]):
-    """
-    Create entry for table with {tableId}
-    """
-    url = f"{hosturl}/api/v2/tables/{tableId}/records"
+        headers = {"xc-token": self.api_token}
 
-    headers = {"xc-token": api_token}
+        response = requests.post(url, data=data, headers=headers)
 
-    response = requests.post(url, data=data, headers=headers)
+        if response.status_code == 200:
+            return "New Row created successfully."
+        else:
+            return response.raise_for_status()
 
-    if response.status_code == 200:
-        return "New Row created successfully."
-    else:
-        return response.raise_for_status()
+    def createLink(
+        self, tableId: str, linkFieldId: str, linkRowId: int, recordRowId: int
+    ):
+        """Create a link in row {linkRowId} to {recordRowId}"""
 
+        url = f"{self.hosturl}/api/v2/tables/{tableId}/links/{linkFieldId}/records/{linkRowId}"
 
-def createLink(tableId: str, linkFieldId: str, linkRowId: int, recordRowId: int):
-    """Create a link in row {linkRowId} to {recordRowId}"""
+        payload = {
+            "Id": recordRowId,
+        }
 
-    url = f"{hosturl}/api/v2/tables/{tableId}/links/{linkFieldId}/records/{linkRowId}"
+        headers = {"xc-token": self.api_token}
 
-    payload = {
-        "Id": recordRowId,
-    }
+        response = requests.post(url, headers=headers, json=payload)
 
-    headers = {"xc-token": api_token}
+        if response.status_code == 200:
+            return "Link created successfully."
+        else:
+            return response.raise_for_status()
 
-    response = requests.post(url, headers=headers, json=payload)
+    def filterEntriesByMonth(self, tableId: str, month: int) -> list:
+        """
+        Return entry in table in with date in {month}
+        """
+        data = json.loads(self.getTableEntries(tableId))
+        july_entries = []
+        for entry in data["list"]:
+            date_str = entry["Date"]
+            date_obj = datetime.strptime(date_str, "%Y-%m-%d")
+            if date_obj.month == month:  # Check if the month is July
+                july_entries.append(entry)
+        return july_entries
 
-    if response.status_code == 200:
-        return "Link created successfully."
-    else:
-        return response.raise_for_status()
+    def idsByMonth(self, tableId: str, month: int) -> list:
+        """
+        Return IDs of entry in table in {month}
 
+        Parameters:
+        tableId (str): Table ID
+        Month (int): Month to filter
+        """
+        data: list = self.filterEntriesByMonth(tableId, month)
 
-def filterEntriesByMonth(tableId: str, month: int) -> list:
-    """
-    Return entry in table in with date in {month}
-    """
-    data = json.loads(getTableEntries(tableId))
-    july_entries = []
-    for entry in data["list"]:
-        date_str = entry["Date"]
-        date_obj = datetime.strptime(date_str, "%Y-%m-%d")
-        if date_obj.month == month:  # Check if the month is July
-            july_entries.append(entry)
-    return july_entries
-
-
-def idsByMonth(tableId: str, month: int) -> list:
-    """
-    Return IDs of entry in table in {month}
-
-    Parameters:
-    tableId (str): Table ID
-    Month (int): Month to filter
-    """
-    data = json.loads(getTableEntries(tableId))
-
-    july_entry_ids = []
-    for entry in data["list"]:
-        date_str = entry["Date"]
-        date_obj = datetime.strptime(date_str, "%Y-%m-%d")
-        if date_obj.month == month:  # Check the month
+        july_entry_ids = []
+        for entry in data:
             july_entry_ids.append(entry["Id"])
-    return july_entry_ids
+
+        return july_entry_ids
