0f73296d9fd77a5dad9df60085b2f6d891100fd6
Stefanomarton <sstefanomarton@gmail.com>
2024-07-25 12:34:01 +0200

    chore: add bigger limit

--- nocodb.py
+++ nocodb.py
@@ -11,30 +11,37 @@ class NocoDB:
         self.headers = {"xc-token": api_token}
         self.baseUrl = f"{self.hosturl}/api/v2"
 
-    def getTableEntries(self, tableId: str, viewID: str = "") -> str:
-        """
-        Get entries for table with {tableId}, optionally use a view with {viewID}
-        """
+    def getTableEntries(self, tableId: str) -> str:
+        """Get entries for table with tableId, use a view with viewID."""
         url = f"{self.baseUrl}/tables/{tableId}/records"
 
-        querystring = {
-            "viewId": viewID,
-        }
+        querystring = {"limit": 1000}
 
         headers = {"xc-token": self.api_token}
 
         response = requests.request("GET", url, headers=headers, params=querystring)
         return response.text
 
-    def createTableRow(self, tableId: str, data: dict[str, str]):
-        """
-        Create entry for table with {tableId}
-        """
+    def createTableRow(self, tableId: str, data: dict):
+        """Create entry for table with {tableId}."""
         url = f"{self.baseUrl}/tables/{tableId}/records"
 
         headers = {"xc-token": self.api_token}
 
         response = requests.post(url, data=data, headers=headers)
+
+        if response.status_code == 200:
+            return "New Row created successfully."
+        else:
+            return response.raise_for_status()
+
+    def updateTableRow(self, tableId: str, data: dict):
+        """Update entry for table with {tableId}."""
+        url = f"{self.baseUrl}/tables/{tableId}/records"
+
+        headers = {"xc-token": self.api_token}
+
+        response = requests.patch(url, data=data, headers=headers)
 
         if response.status_code == 200:
             return "New Row created successfully."
